d4785ff26148e07a2279f0c6668026d14489c029
Denis N <nov_den@mail.ru>
2020-11-02 22:26:46 +0300

    new

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "stdlib.h"
+#include "math.h"
+
+#include <stdio.h>
+
+int main()
+{
+      double x, y,
+            a, b, c;
+
+    printf("Vvedite x:");
+    scanf("%lf",&x);
+
+    a=2* (cos(x- (M_PI/6))) + sqrt(2); // числитель
+
+    c=pow(x,2);
+
+    b=1/(2* (log(x) ) ) + (pow(sin(c),2)); // знаменатель
+
+    y=(a/b) * (exp(3*x));
+
+    printf("a = %lf:\n", a);
+    printf("b = %lf:\n", b);
+    printf("y = %lf:\n", y);
+
+    return 0;
+}
